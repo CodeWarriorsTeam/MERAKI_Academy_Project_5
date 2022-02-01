c68b3a0a25b9a023caffd1342d64161cae311b0c
AnasNaamneh <naamnih.anas@gmail.com>
2022-02-01 17:21:39 +0200

    createcase

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use("*", (req, res) => res.status(404).json("NO content at this path"));
 
 
 ////batool
@@ -29,6 +28,12 @@ app.use("*", (req, res) => res.status(404).json("NO content at this path"));
 
 
 /////naamneh
+const casesRouter = require("./routes/cases");
+
+app.use("/",casesRouter);
+
+
+app.use("*", (req, res) => res.status(404).json("NO content at this path"));
 
 
 const PORT = 5000;
