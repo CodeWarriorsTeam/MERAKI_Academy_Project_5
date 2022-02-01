aacb28a2eb5c29fba2c9da5224fb50df7da7384e
AnasNaamneh <naamnih.anas@gmail.com>
2022-02-01 21:48:18 +0200

    modify case controller

--- backend/controllers/cases.js
+++ backend/controllers/cases.js
@@ -16,80 +16,119 @@ const createNewCase = (req, res) => {
     res.status(201).json({
       success: true,
       message: `Case Created`,
+      result: result
     });
   });
 };
 
 const getAllCases = (req, res) => {
-  const query = `SELECT * FROM cases`;
+  const query = `SELECT * FROM cases WHERE is_deleted=0`;
 
   connection.query(query, (err, result) => {
     if (err) {
-      res.status(500).json({
+    return  res.status(500).json({
         success: false,
         message: `Server Error`,
       });
     }
 
+if (!result[0]){
+  return res.status(200).json({
+    success:false,
+    message: `no cases yet`
+  })
+}
+
+
     res.status(200).json({
       success: true,
-      message: [result],
+      message: result
+    });
+  });
+};
+
+const getCaseById = (req, res) => {
+  const query = `SELECT * FROM cases WHERE id=? and is_deleted=0`;
+
+  const data = [req.params.id];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+    return  res.status(500).json({
+        success: false,
+        message: `Server Error`,
+      });
+    }
+
+    if (!result[0]){
+      return res.status(200).json({
+        success:false,
+        message: `No case at id ${data}`
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      message: `The Case with id ${data}`,
+      result: result
+    });
+  });
+};
+
+const updateCaseById = (req, res) => {
+  const query = `UPDATE cases SET? WHERE id=?`;
+
+  const data = [req.body, req.params.id];
+
+  connection.query(query, data, (err, result) => {
+    if (err) {
+     return res.status(500).json({
+        success: false,
+        message: `Server Error`,
+      });
+    }
+
+    if (!result.affectedRows){
+     return res.status(404).json({
+        success:false,
+        message: `The Case is not Found`,
+
+    })
+  }
+    res.status(201).json({
+      success: true,
+      message: `Case Updated `,
     });
   });
 };
 
 
-const getCaseById = (req,res)=>{
+const deleteCaseById = (req,res)=>{
+const query = `UPDATE cases SET is_deleted=1 WHERE id=?` 
 
-  const query = `SELECT * FROM cases WHERE id=?`;
-
-
-  const data = [req.params.id]
-  connection.query(query,data,(err,result)=>{
-
-    if (err){
-      res.status(404).json({
-        success:false,
-        message: `The Case not Found`
-      })
-    }
-
-    res.status(200).json({
-       success:true,
-       message: `The Case with id ${data}`,
-       result: [result]
-       
-    })
-  })
-}
-
-
-
-
-
-
-
-
-
-
-const updateCaseById = (req,res)=>{
-const query = `UPDATE cases SET? WHERE id=?`
-
-const data = [req.body,req.params.id]
+const data = [req.params.id];
 
 connection.query(query,data,(err,result)=>{
-
-if (err){
-    res.status(404).json({
-        success:false,
-        message:`The Case is not Found`
+  if (err){
+    console.log(err);
+   return res.status(500).json({
+      success:false,
+      message: `Server Error`
     })
-}
+  }
 
-    res.status(201).json({
-        success:true,
-        message:`Case Updated `
-    })
+  if (!result.affectedRows){
+return res.status(404).json({
+  success:false,
+  message: `The Case with ${data} not Found`
+
+})
+  }
+
+  res.status(200).json({
+    success:true,
+    message: `Succeeded to delete case with id ${data}`
+
+  })
 })
 }
 
@@ -98,5 +137,6 @@ module.exports = {
   createNewCase,
   getAllCases,
   getCaseById,
-  updateCaseById
+  updateCaseById,
+  deleteCaseById
 };
--- backend/routes/cases.js
+++ backend/routes/cases.js
@@ -1,13 +1,14 @@
 const express = require("express");
 
-const {createNewCase,getAllCases,getCaseById,updateCaseById} = require("../controllers/cases")
+const {createNewCase,getAllCases,getCaseById,updateCaseById,deleteCaseById} = require("../controllers/cases")
 
 const casesRouter = express.Router();
 
 casesRouter.post("/",createNewCase);
 casesRouter.get("/",getAllCases);
-casesRouter.get("/:id",getCaseById)
-casesRouter.put("/:id",updateCaseById)
+casesRouter.get("/:id",getCaseById);
+casesRouter.put("/:id",updateCaseById);
+casesRouter.delete("/:id",deleteCaseById);
 
 
 module.exports = casesRouter;
