53bf4ef7bfb402795a0c246d3a2ecec7a8e331ed
AnasNaamneh <naamnih.anas@gmail.com>
2022-02-01 20:16:36 +0200

    getcasebyid

--- backend/controllers/cases.js
+++ backend/controllers/cases.js
@@ -38,6 +38,40 @@ const getAllCases = (req, res) => {
   });
 };
 
+
+const getCaseById = (req,res)=>{
+
+  const query = `SELECT * FROM cases WHERE id=?`;
+
+
+  const data = [req.params.id]
+  connection.query(query,data,(err,result)=>{
+
+    if (err){
+      res.status(404).json({
+        success:false,
+        message: `The Case not Found`
+      })
+    }
+
+    res.status(200).json({
+       success:true,
+       message: `The Case with id ${data}`,
+       result: [result]
+       
+    })
+  })
+}
+
+
+
+
+
+
+
+
+
+
 const updateCaseById = (req,res)=>{
 const query = `UPDATE cases SET? WHERE id=?`
 
@@ -63,5 +97,6 @@ if (err){
 module.exports = {
   createNewCase,
   getAllCases,
+  getCaseById,
   updateCaseById
 };
