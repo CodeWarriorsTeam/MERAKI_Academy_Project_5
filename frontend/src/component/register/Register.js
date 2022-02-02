66422c5dc986cfe2cd3931d3b06c89f39d0498f6
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-02-02 10:37:25 +0200

    add css file for register

--- frontend/src/component/register/Register.js
+++ frontend/src/component/register/Register.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Register.css";
-
+//////
 const Register = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
