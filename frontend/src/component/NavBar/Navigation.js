7c9d32d9184d83d5559a97f0eac16f6926db17e6
BatoolAbuSneneh <batoolabusneneh56@gmail.com>
2022-02-06 23:02:19 +0200

    create adminPanel component

--- frontend/src/component/NavBar/Navigation.js
+++ frontend/src/component/NavBar/Navigation.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { logoutUser } from "../../reducer/login";
 import { useNavigate } from "react-router-dom";
-import {MdSettingsPhone,MdSearch} from "react-icons/md"
-import {HiOutlineMail} from "react-icons/hi"
+import { MdSettingsPhone, MdSearch } from "react-icons/md";
+import { HiOutlineMail } from "react-icons/hi";
 import "./Navigation.css";
-const Navigation = ({
+const Navigation = ({isAdmin,
   setSearchCase,
   setCategory,
   setAllCase,
@@ -29,75 +29,96 @@ const Navigation = ({
 
   return (
     <>
-    <div><a className="num"><MdSettingsPhone className="phone"/> +00012345678</a>  <a className="word"><HiOutlineMail className="email22"/>safe.house@gmail.com</a></div>
+      <div>
+        <a className="num">
+          <MdSettingsPhone className="phone" /> +00012345678
+        </a>{" "}
+        <a className="word">
+          <HiOutlineMail className="email22" />
+          safe.house@gmail.com
+        </a>
+      </div>
       {/* <nav className="NavBar">     */}
-        {/* <div className="logo">
+      {/* <div className="logo">
           <h1 id="headerLogo" className="animate__animated animate__bounce animate__infinites">Safe House</h1> 
         </div> */}
-        <div className="menu">
-          <a><img className="logo" src="https://tse3.mm.bing.net/th?id=OIP.T9dXwfMb2pOdT1tuFBKYigHaGZ&pid=Api&P=0&w=201&h=174"/></a>
-          <Link className="safeHouseLink" to="/"> <p className="safeHouse">SAFE HOUSE</p></Link>
-         {/* <MdSearch className="iconSearch"/>  */}
-         <input
-            id="searchInput"
-            type="text"
-            placeholder="Search here...."
-            onChange={(e) => {
-              setSearchCase(e.target.value);
-            }}
+      <div className="menu">
+        <a>
+          <img
+            className="logo"
+            src="https://tse3.mm.bing.net/th?id=OIP.T9dXwfMb2pOdT1tuFBKYigHaGZ&pid=Api&P=0&w=201&h=174"
           />
-          <Link to="/">
-            <a className="Home">Home</a>
-          </Link>
-          <Link
-            to="/allcases"
-            onClick={() => {
-              setAllCase(true);
-              setCategory(false);
-            }}
-          >
-            <a id="AllCases">All Cases</a>{" "}
-          </Link>
-          <Link
-            to="/allcases"
-            onClick={() => {
-              setCategory(`education`);
-              setAllCase(false);
-            }}
-          >
-            <a className="education">Education</a>{" "}
-          </Link>
-          <Link
-            to="/allcases"
-            onClick={() => {
-              setCategory(`kids`);
-              setAllCase(false);
-            }}
-          >
-            <a id="kids">Kids</a>{" "}
-          </Link>
-          <Link className="newcase" to="/newcase">
-            <a id="newCase">New Case</a>{" "}
-          </Link>
-</div>
+        </a>
+        <Link className="safeHouseLink" to="/">
+          {" "}
+          <p className="safeHouse">SAFE HOUSE</p>
+        </Link>
+        {/* <MdSearch className="iconSearch"/>  */}
+        <input
+          id="searchInput"
+          type="text"
+          placeholder="Search here...."
+          onChange={(e) => {
+            setSearchCase(e.target.value);
+          }}
+        />
+        <Link to="/">
+          <a className="Home">Home</a>
+        </Link>
+        <Link
+          to="/allcases"
+          onClick={() => {
+            setAllCase(true);
+            setCategory(false);
+          }}
+        >
+          <a id="AllCases">All Cases</a>{" "}
+        </Link>
+        <Link
+          to="/allcases"
+          onClick={() => {
+            setCategory(`education`);
+            setAllCase(false);
+          }}
+        >
+          <a className="education">Education</a>{" "}
+        </Link>
+        <Link
+          to="/allcases"
+          onClick={() => {
+            setCategory(`kids`);
+            setAllCase(false);
+          }}
+        >
+          <a id="kids">Kids</a>{" "}
+        </Link>
+        {isAdmin ? (
+          <>
+          <Link className="admin" to="/admin">Admin Panel</Link>
+            {/* <Link className="newcase" to="/newcase">
+          <a id="newCase">New Case</a>{" "}
+        </Link> */}</>
+        ) : (
           <></>
-          {state.isLoggedIn || userId ? (
-            <a id="Logout" onClick={logout}>
-              Logout
-            </a>
-           
-          ) : ( 
-            
-            <>
-              <Link className="register" to="/register">
-                <a id="RegisterNav">Sign Up</a>
-              </Link>
-              <Link className="login" to="/login">
-               Login
-              </Link>
-            </>
-          )}
-        
+        )}
+      
+      </div>
+      <></>
+      {state.isLoggedIn || userId ? (
+        <a id="Logout" onClick={logout}>
+          Logout
+        </a>
+      ) : (
+        <>
+          <Link className="register" to="/register">
+            <a id="RegisterNav">Sign Up</a>
+          </Link>
+          <Link className="login" to="/login">
+            Login
+          </Link>
+        </>
+      )}
+
       {/* </nav> */}
     </>
   );
