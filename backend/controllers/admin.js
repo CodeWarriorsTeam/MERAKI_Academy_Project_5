aca41ca30ac4473ccbdf0ab230640023032f7448
HamzaShahrori <hamza.shahrorih@gmail.com>
2022-02-08 23:18:31 +0200

    countCasesIsclose

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -27,6 +27,32 @@ const getAllCasesAdminPanel = (req, res) => {
   };
 
 
+  const getSum = (req, res) => {
+    const query = `SELECT  COUNT(TheAmountRequired) AS "countEducation" FROM cases   WHERE cases.category='education' && cases.is_deleted=0 &&cases.TheAmountRequired=0`;
+  
+    connection.query(query, async (err, result) => {
+      if (err) {
+        return res.status(500).json({
+          success: false,
+          message: `Server Error`,
+        });
+      }
+  
+      if (!result[0]) {
+        return res.status(404).json({
+          success: false,
+          message: `no cases yet`,
+        });
+      }
+  
+      res.status(200).json({
+        success: true,
+        message: `all cases`,
+        result: result,
+      });
+    });
+  };
+
 
   
-module.exports = { getAllCasesAdminPanel };
+module.exports = { getAllCasesAdminPanel,getSum };
